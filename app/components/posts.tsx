466e07327040039b064631195674f7b7b0354052
Dashvin Singh <circleline10@gmail.com>
2026-01-07 11:48:55 -0500

    writing publicly

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -18,11 +18,11 @@ export function BlogPosts() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
+            className="flex mb-4"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+            <div className="w-full flex flex-col md:flex-row items-start space-y-1 md:space-y-0 md:space-x-6">
+              <p className="text-neutral-600 dark:text-neutral-400 text-xs md:text-base md:min-w-[100px] md:flex-shrink-0 tabular-nums">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
